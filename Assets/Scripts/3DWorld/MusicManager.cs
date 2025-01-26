7329ef4a73c03164ff698ebc65bbd50e8325dc89
vailhou <huugolio@gmail.com>
2025-01-26 15:43:00 +0200

    Fixed music cross fade bug

--- Assets/Scripts/3DWorld/MusicManager.cs
+++ Assets/Scripts/3DWorld/MusicManager.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using UnityEngine;
 
 public class MusicManager : MonoBehaviour
@@ -8,6 +9,9 @@ public class MusicManager : MonoBehaviour
     [SerializeField] private AudioSource gameMusic2D;
 
     private readonly float crossFadeTime = 0.5f;
+    private bool canFadeToBubbleheads = true;
+    private Coroutine fadeIn;
+    private Coroutine fadeOut;
 
     private void Awake()
     {
@@ -24,13 +28,60 @@ public class MusicManager : MonoBehaviour
 
     public void Play3DWorldMusic()
     {
-        gameMusic2D.FadeOut(crossFadeTime);
-        worldMusic3D.FadeIn(crossFadeTime);
+        if (fadeIn != null)
+        {
+            StopCoroutine(fadeIn);
+        }
+        if (fadeOut != null)
+        {
+            StopCoroutine(fadeOut);
+        }
+        fadeOut = StartCoroutine(FadeOut(gameMusic2D, crossFadeTime));
+        fadeIn = StartCoroutine(FadeIn(worldMusic3D, crossFadeTime));
     }
 
     public void Play2DGameMusic()
     {
-        worldMusic3D.FadeOut(crossFadeTime);
-        gameMusic2D.FadeIn(crossFadeTime);
+                if (fadeIn != null)
+        {
+            StopCoroutine(fadeIn);
+        }
+        if (fadeOut != null)
+        {
+            StopCoroutine(fadeOut);
+        }
+        fadeOut = StartCoroutine(FadeOut(worldMusic3D, crossFadeTime));
+        fadeIn = StartCoroutine(FadeIn(gameMusic2D, crossFadeTime));
+    }
+
+    private IEnumerator FadeOut(AudioSource a, float duration)
+    {
+        //float startVolume = a.volume;
+        float startVolume = 1;
+
+        while (a.volume > 0)
+        {
+            a.volume -= startVolume * Time.deltaTime / duration;
+            yield return new WaitForEndOfFrame();
+        }
+
+        a.Stop();
+        a.volume = startVolume;
+    }
+
+    private static IEnumerator FadeIn(AudioSource a, float duration)
+    {
+        float startVolume = 0.2f;
+
+        a.volume = 0;
+        a.Play();
+
+        while (a.volume < 1.0f)
+        {
+            a.volume += startVolume * Time.deltaTime / duration;
+            yield return null;
+        }
+
+        a.volume = 1f;
     }
 }
